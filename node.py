bd90b745769cf9e6a6c25eba276fc5f02d20b80b
jc2 <juancamiloceron@gmail.com>
2021-06-15 11:49:15 -0500

    Refactoring AIOHTTP Session

--- node.py
+++ node.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractclassmethod
+from enum import Enum
 
+import aiohttp
 import tenacity
 
 
@@ -18,38 +20,54 @@ class NodeAction(ABC):
         pass
 
 
+class NodeActionState(Enum):
+    READY = 1
+    NOT_NEEDED = 2
+    DONE = 3
+    ROLLED_BACK = 4
+    ERROR = 5
+
+
 class CreateGroup(NodeAction):
 
-    def __init__(self, session, node):
-        self.session = session
+    def __init__(self, node):
         self.node = node
+        self.status = None
 
     @tenacity.retry(stop=tenacity.stop_after_attempt(3))
     async def fordward(self, group_name):
-        return await NodeClient.create_group(self.session, self.node, group_name)
+        return await NodeClient.create_group(self.node, group_name)
 
     @tenacity.retry(stop=tenacity.stop_after_attempt(3))
     async def backward(self, group_name):
-        return await NodeClient.delete_group(self.session, self.node, group_name)
+        return await NodeClient.delete_group(self.node, group_name)
 
     @tenacity.retry(stop=tenacity.stop_after_attempt(3))
     async def get_current_status(self, group_name):
-        return await NodeClient.get_group(self.session, self.node, group_name)
+        try:
+            response = await NodeClient.get_group(self.node, group_name)
+        except NodeGroupNotFound:
+            self.status = NodeActionState.READY
+        except NodeError:
+            self.status = NodeActionState.ERROR
+        else:
+            self.status = NodeActionState.NOT_NEEDED
+            return response
 
 
 class DeleteGroup(NodeAction):
 
     # @tenacity.retry(stop=tenacity.stop_after_attempt(3))
-    async def fordward(self, session, node, group_name):
-        return await NodeClient.delete_group(session, node, group_name)
+    async def fordward(self, node, group_name):
+        return await NodeClient.delete_group(node, group_name)
 
     # @tenacity.retry(stop=tenacity.stop_after_attempt(3))
-    async def backward(self, session, node, group_name):
-        return await NodeClient.create_group(session, node, group_name)
+    async def backward(self, node, group_name):
+        return await NodeClient.create_group(node, group_name)
 
     # @tenacity.retry(stop=tenacity.stop_after_attempt(3))
-    async def get_current_status(self, session, node, group_name):
-        return await NodeClient.get_group(session, node, group_name)
+    async def get_current_status(self, node, group_name):
+        return await NodeClient.get_group(node, group_name)
 
 
 class NodeError(Exception):
@@ -64,30 +82,33 @@ class NodeGroupNotFound(NodeError):
 class NodeClient():
 
     @staticmethod
-    async def create_group(session, node, group_name):
-        print(f"Creating on {node}")
-        response = await session.post(f"{node}/v1/group", json={"groupId": group_name})
-        if not response.status == 201:
-            raise NodeError(node)
-        print(f"Finishing creation on {node}")
-        return response
+    async def create_group(node, group_name):
+        async with aiohttp.ClientSession() as session:
+            print(f"Creating on {node}")
+            response = await session.post(f"{node}/v1/group", json={"groupId": group_name})
+            if not response.status == 201:
+                raise NodeError(node)
+            print(f"Finishing creation on {node}")
+            return response
 
     @staticmethod
-    async def delete_group(session, node, group_name):
-        print(f"Deleting on {node}")
-        response = await session.delete(f"{node}/v1/group", json={"groupId": group_name})
-        if not response.status == 200:
-            raise NodeError(node)
-        print(f"Deleting creation on {node}")
-        return response
+    async def delete_group(node, group_name):
+        async with aiohttp.ClientSession() as session:
+            print(f"Deleting on {node}")
+            response = await session.delete(f"{node}/v1/group", json={"groupId": group_name})
+            if not response.status == 200:
+                raise NodeError(node)
+            print(f"Deleting creation on {node}")
+            return response
 
     @staticmethod
-    async def get_group(session, node, group_name):
-        print(f"Getting from {node}")
-        response = await session.get(f"{node}/v1/group/{group_name}")
-        if response.status == 404:
-            raise NodeGroupNotFound(node)
-        elif not response.status == 200:
-            raise NodeError(node)
-        print(f"Finishing getting from {node}")
-        return response
+    async def get_group(node, group_name):
+        async with aiohttp.ClientSession() as session:
+            print(f"Getting from {node}")
+            response = await session.get(f"{node}/v1/group/{group_name}")
+            if response.status == 404:
+                raise NodeGroupNotFound(node)
+            elif not response.status == 200:
+                raise NodeError(node)
+            print(f"Finishing getting from {node}")
+            return response
--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -9,13 +9,12 @@ from node import NodeClient, NodeError, NodeGroupNotFound
 
 def test_node_create_group():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.post(node+"/v1/group", status=201, payload={"groupId": "group_1"})
 
-        resp = loop.run_until_complete(NodeClient.create_group(session, node, "group_1"))
+        resp = loop.run_until_complete(NodeClient.create_group(node, "group_1"))
         data = loop.run_until_complete(resp.json())
 
         assert data == {"groupId": "group_1"}
@@ -24,27 +23,25 @@ def test_node_create_group():
 
 def test_node_create_group_exception():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.post(node+"/v1/group", status=500, payload={"groupId": "group_1"})
         with pytest.raises(NodeError):
-            loop.run_until_complete(NodeClient.create_group(session, node, "group_1"))
+            loop.run_until_complete(NodeClient.create_group(node, "group_1"))
         mocker.post(node+"/v1/group", status=400, payload={"groupId": "group_1"})
         with pytest.raises(NodeError):
-            loop.run_until_complete(NodeClient.create_group(session, node, "group_1"))
+            loop.run_until_complete(NodeClient.create_group(node, "group_1"))
 
 
 def test_node_get_group():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.get(node+"/v1/group/group_1", status=200, payload={"groupId": "group_1"})
 
-        resp = loop.run_until_complete(NodeClient.get_group(session, node, "group_1"))
+        resp = loop.run_until_complete(NodeClient.get_group(node, "group_1"))
         data = loop.run_until_complete(resp.json())
 
         assert data == {"groupId": "group_1"}
@@ -53,27 +50,25 @@ def test_node_get_group():
 
 def test_node_get_group_exception():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.get(node+"/v1/group/group_1", status=404, payload={"groupId": "group_1"})
         with pytest.raises(NodeGroupNotFound):
-            loop.run_until_complete(NodeClient.get_group(session, node, "group_1"))
+            loop.run_until_complete(NodeClient.get_group(node, "group_1"))
         mocker.get(node+"/v1/group/group_1", status=500, payload={"groupId": "group_1"})
         with pytest.raises(NodeError):
-            loop.run_until_complete(NodeClient.get_group(session, node, "group_1"))
+            loop.run_until_complete(NodeClient.get_group(node, "group_1"))
 
 
 def test_node_delete_group():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.delete(node+"/v1/group", status=200, payload={"groupId": "group_1"})
 
-        resp = loop.run_until_complete(NodeClient.delete_group(session, node, "group_1"))
+        resp = loop.run_until_complete(NodeClient.delete_group(node, "group_1"))
         data = loop.run_until_complete(resp.json())
 
         assert data == {"groupId": "group_1"}
@@ -82,10 +77,9 @@ def test_node_delete_group():
 
 def test_node_delete_group_exception():
     loop = asyncio.get_event_loop()
-    session = aiohttp.ClientSession()
 
     with aioresponses() as mocker:
         node = "node1.cluster.com"
         mocker.delete(node+"/v1/group", status=500, payload={"groupId": "group_1"})
         with pytest.raises(NodeError):
-            loop.run_until_complete(NodeClient.delete_group(session, node, "group_1"))
+            loop.run_until_complete(NodeClient.delete_group(node, "group_1"))
